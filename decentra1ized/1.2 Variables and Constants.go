da79386007fb628b556aac63305a0723f67901b1
Chaerin Kim <decentralized0212@gmail.com>
2021-09-29 21:05:29 +0900

    1주차 업로드

---
+++ decentra1ized/1.2 Variables and Constants.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/decentra1ized/golang_study/decentra1ized"
+import "fmt"
+
+func main() {
+	something.SayHello()
+	name := "nico"
+	name = "lynn"
+	fmt.Println(name)
+}
