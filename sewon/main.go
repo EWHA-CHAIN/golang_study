386d7763d74e8cf26a2e90fa52e41c2e1cdd44fb
sw-develop <psw00719@naver.com>
2021-10-06 12:11:27 +0900

    [추가] use map & error handling for searching value

--- sewon/main.go
+++ sewon/main.go
@@ -1,21 +1,16 @@
 package main // 어떤 패키지를 사용하는지 명시해줌, main.go 파일의 경우 컴파일을 위해서 필요한 것임(필수)
 import (
 	"fmt"
-	"github.com/sw-develop/learngo/golang_study/sewon/accounts"
+	"github.com/sw-develop/learngo/golang_study/sewon/dictEx"
 )
 
 func main() {
-	account := accounts.NewAccount("meme") // 변수 선언 축약형 - var account *account.Account = account.NewAccount(" ")와 동일함
-	fmt.Println(*account)
+	dictionary := dictEx.Dictionary{"first": "first word"}
+	definition, err := dictionary.Search("first") // definition, err := dictionary["first"]와 동일한 의미인데, 저렇게 작성하는게 더 좋은 코드 형태임
 
-	account.Deposit(10)
-	fmt.Println(account.GetBalance())
-
-	err := account.WithDraw(20)
-	if err != nil { // error handling in Go (직접 작성해줘야 함)
+	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
 	}
-	fmt.Println(account.GetBalance(), account.GetOwner())
-
-	fmt.Println(account)
 }
