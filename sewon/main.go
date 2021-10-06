ae588bed0f3633d685b6537621901d35381fea73
sw-develop <psw00719@naver.com>
2021-10-06 12:53:45 +0900

    [추가] use update, delete method

--- sewon/main.go
+++ sewon/main.go
@@ -5,20 +5,40 @@ import (
 )
 
 func main() {
-	dictionary := dictEx.Dictionary{"first": "first word"}
-	def1, err1 := dictionary.Search("first") // definition, err := dictionary["first"]와 동일한 의미인데, 저렇게 작성하는게 더 좋은 코드 형태임
 
-	if err1 != nil {
-		fmt.Println(err1)
+	dictionary := dictEx.Dictionary{"first": "first word"}
+	//	def1, err1 := dictionary.Search("first") // definition, err := dictionary["first"]와 동일한 의미인데, 저렇게 작성하는게 더 좋은 코드 형태임
+	/*
+		if errs1 != nil {
+			fmt.Println(err1)
+		} else {
+			fmt.Println(def1)
+		}
+
+		err2 := dictionary.AddWord("first", "second word")
+		if err2 != nil {
+			fmt.Println(err2)
+		} else {
+			def2, _ := dictionary.Search("second")
+			fmt.Println(def2)
+		}
+	*/
+
+	baseWord := "fourth"
+	dictionary.AddWord(baseWord, "fourth word")
+	err3 := dictionary.UpdateWord(baseWord, "update next word")
+	if err3 != nil {
+		fmt.Println(err3)
 	} else {
-		fmt.Println(def1)
+		value, _ := dictionary.Search(baseWord)
+		fmt.Println(value)
 	}
 
-	err2 := dictionary.AddWord("first", "second word")
-	if err2 != nil {
-		fmt.Println(err2)
+	dictionary.DeleteWord(baseWord)
+	value, err := dictionary.Search(baseWord)
+	if err != nil {
+		fmt.Println(err)
 	} else {
-		def2, _ := dictionary.Search("second")
-		fmt.Println(def2)
+		fmt.Println(value)
 	}
 }
