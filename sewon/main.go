bd2b870bca3b13779e3925e68f7bdfbd4cd496f2
sw-develop <psw00719@naver.com>
2021-10-12 01:01:52 +0900

    [추가] goquery를 사용해 pagination된 각 페이지의 job들의 id, title, location 정보 가져오기

--- sewon/main.go
+++ sewon/main.go
@@ -2,48 +2,92 @@ package main
 
 import (
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io"
+	"log"
 	"net/http"
+	"strconv"
 )
 
-type requestResult struct { // http 요청 값을 저장할 구조체
-	url    string
-	status string
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
 }
+
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
-	results := make(map[string]string)
-	channel := make(chan requestResult)
+	totalPages := getPages()
 
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
 	}
-	for _, url := range urls {
-		go hitURL(url, channel) // using goroutine
-	}
-
-	for i := 0; i < len(urls); i++ {
-		result := <-channel
-		results[result.url] = result.status
-	}
-
-	for url, status := range results {
-		fmt.Println(url, status)
-	}
-
 }
 
-func hitURL(url string, channel chan<- requestResult) { // chan<- : Send Only
-	res, err := http.Get(url)
-	status := "OK"
-	if err != nil || res.StatusCode >= 400 {
-		status = "FAILED"
+func getPage(pageNum int) { // pagination된 페이지 호출
+	pageURL := baseURL + "&start=" + strconv.Itoa(pageNum*50)
+	fmt.Println("Requesting: ", pageURL)
+
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkHttpStatus(res)
+
+	defer func(Body io.ReadCloser) { // error handling for io.ReadCloser
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(res.Body)
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchJobs := doc.Find(".tapItem")
+
+	searchJobs.Each(func(i int, selection *goquery.Selection) {
+		id, _ := selection.Attr("data-jk")
+		title := selection.Find("h2>span").Text()
+		location := selection.Find(".companyLocation").Text()
+		fmt.Println(id, title, location)
+	})
+}
+
+func getPages() int { // pagination 수 반환
+	pages := 0
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkHttpStatus(res)
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(res.Body)
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, selection *goquery.Selection) {
+		// For each item found, count pages
+		pages = selection.Find("a").Length() // <a> 태그 찾기
+	})
+
+	return pages
+}
+
+func checkErr(err error) { // 에러 처리
+	if err != nil {
+		log.Fatalln(err)
 	}
-	channel <- requestResult{url: url, status: status}
+}
+
+func checkHttpStatus(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status:", res.StatusCode)
+	}
 }
